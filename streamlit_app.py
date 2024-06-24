41fc99f716d7a0234823a34f9920253db7184402
DavidL10189 <dlawrence188843@troy.edu>
2024-06-24 13:11:29 +0000

    Initial Commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,96 @@
-import altair as alt
-import numpy as np
-import pandas as pd
+#Research & Development App - OS Natural Language Interface
+
+#Modules
 import streamlit as st
+import os
+import google.generativeai as ggi
+from langchain_community.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.chains import RetrievalQA
+from langchain_community.vectorstores import Chroma
+from langchain.chains.question_answering import load_qa_chain
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain.schema.runnable import RunnableMap
+from langchain_community.vectorstores import DocArrayInMemorySearch
+from langchain_core.prompts import ChatPromptTemplate
 
+#Variables to hold our different documents to be used.
+fileTroy = "RAGDocuments/prompt_answer.csv"
+fileOS = "RAGDocuments/prompt_OS_answer.csv"
+
+#Get API Key from Secrets file into a variable.
+apikey = st.secrets["API_KEY"]
+
+#A function to load a document.
+def DocLoader(fileName):
+   loader = CSVLoader(fileName, csv_args={'delimiter':','})
+   return loader.load()
+
+#A function to split a document.
+#Chunking sizes chosen should cover entire lines and overlap parts of contiguous lines
+def DocSplitter(document):
+   splitter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=200)
+   return splitter.split_documents(document)
+
+#Load our documents used for RAG
+loadedTroy = DocLoader(fileTroy)
+loadedOS = DocLoader(fileOS)
+#Split our documents
+troy_Split = DocSplitter(loadedTroy)
+OS_Split = DocSplitter(loadedOS)
+
+#Create an embeddings object.
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key=apikey)
+
+#Create a vector store from the text in the CSV file
+vectorstore = DocArrayInMemorySearch.from_texts(troy_Split,embedding=embeddings)
+
+retriever = vectorstore.as_retriever()
+
+#Text to display status to the user
+headerDisplay = "Hello"
+detailDisplay = "Please ask a question above"
+
+st.title("Gemini assistant & :red[NLP OS I/F R&D]")
+
+#Create Gemini AI object. Apply the Gemini API Key. Set a loose temperature.
+model = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=apikey,temperature=0.9,
+                                  convert_system_message_to_human=True)
+
+#Prompt the user to input their request.
+userQuestion = st.text_area("You can ask general questions, questions about Troy University, and in the future interface with your OS! Press **CTRL+Enter** to send your question.")
+
+#Clear the section which displays results from Gemini.
+responseTitle = st.empty()
+responseTitle.write("")
+responseBody = st.empty()
+responseBody.write("")
+
+#The prompt template and prompt.
+template = """Answer the question based on the following context:
+{context}
+
+Question: {question}
 """
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+prompt = ChatPromptTemplate.from_template(template)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+#Functionality to perform the communication with the API and then display the results.
+if userQuestion:
+   responseTitle.write("Processing")
+   responseBody.write("")   
+   chain = RunnableMap({
+      "context": lambda x: retriever.get_relevant_documents(x["question"]),
+      "question": lambda x: x["question"]
+   }) | prompt | model      
+   output = chain.invoke({"question": userQuestion})
+   responseTitle.write("")
+   responseBody.write(output.content)
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))

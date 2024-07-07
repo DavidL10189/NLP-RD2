a0b06efa676fa79d728b9dcb297450abf8e6b8fb
DavidL10189 <dlawrence188843@troy.edu>
2024-07-07 15:19:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,7 @@ from langchain_community.vectorstores import DocArrayInMemorySearch
 from langchain_core.prompts import ChatPromptTemplate
 from pathlib import Path
 
+st.Write(datetime.value)
 
 #Variables to hold our different documents to be used.
 fileTroy = "/mount/src/nlp-rd2/InputDocs/prompt_answer.csv"

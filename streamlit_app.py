5de035d5a8931445cd068c74572ce4f8963ef939
DavidL10189 <dlawrence188843@troy.edu>
2024-07-07 20:50:45 +0000

    DT test

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,9 @@ from langchain.schema.runnable import RunnableMap
 from langchain_community.vectorstores import DocArrayInMemorySearch
 from langchain_core.prompts import ChatPromptTemplate
 from pathlib import Path
+import datetime
+
+st.Write(datetime.datetime.now)
 
 #Variables to hold our different documents to be used.
 fileTroy = "/mount/src/nlp-rd2/InputDocs/prompt_answer.csv"
